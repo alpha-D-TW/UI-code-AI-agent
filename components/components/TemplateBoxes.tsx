f5b294c141134e06ca5a380c7fa42ce9370b663b
Yang Zhengqing <1370483456@qq.com>
2024-03-01 10:53:33 +0800

    [zhengqing] *N/A feat: change as antd

--- components/components/TemplateBoxes.tsx
+++ components/components/TemplateBoxes.tsx
@@ -11,50 +11,48 @@ function TemplateBoxes() {
 
   return (
     <>
-        {templates.list.map((template) => {
-          return (
-            <div
-              onClick={() => {
-                setSettings({
-                  ...settings,
-                  generatedCodeConfig: GeneratedCodeConfig.HTML_TAILWIND,
-                });
-                router.push(`/editor/${template.id}`);
-              }}
-              key={template.id}
-              className="bg-white shadow-lg rounded-lg p-4 flex flex-col hover:ring ring-black relative border"
-            >
-              <div className="flex-1">
-                <div className="aspect-[1376/768]">
-                  <img
-                    className="w-full"
-                    src={template.imageUrl}
-                    alt={template.title}
-                  />
-                </div>
-                <h3 className="mt-4 text-sm text-gray-700">
-                  {template.title}
-                </h3>
-                <p className="mt-1 text-lg font-medium text-gray-900">
-                  {template.description}
-                </p>
+      {templates.list.map((template) => {
+        return (
+          <div
+            onClick={() => {
+              setSettings({
+                ...settings,
+                generatedCodeConfig: GeneratedCodeConfig.REACT_ANTD,
+              });
+              router.push(`/editor/${template.id}`);
+            }}
+            key={template.id}
+            className="bg-white shadow-lg rounded-lg p-4 flex flex-col hover:ring ring-black relative border"
+          >
+            <div className="flex-1">
+              <div className="aspect-[1376/768]">
+                <img
+                  className="w-full"
+                  src={template.imageUrl}
+                  alt={template.title}
+                />
               </div>
-              {template.fromUrl && (
-                <a
-                  onClick={(e) => {
-                    e.stopPropagation();
-                  }}
-                  className="absolute right-2 top-2"
-                  href={template.fromUrl}
-                  target="_blank"
-                >
-                  <FaGithubSquare className="text-xl" />
-                </a>
-              )}
+              <h3 className="mt-4 text-sm text-gray-700">{template.title}</h3>
+              <p className="mt-1 text-lg font-medium text-gray-900">
+                {template.description}
+              </p>
             </div>
-          );
-        })}
-  </>
+            {template.fromUrl && (
+              <a
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+                className="absolute right-2 top-2"
+                href={template.fromUrl}
+                target="_blank"
+              >
+                <FaGithubSquare className="text-xl" />
+              </a>
+            )}
+          </div>
+        );
+      })}
+    </>
   );
 }
 
